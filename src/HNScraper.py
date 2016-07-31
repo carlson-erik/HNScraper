bbc290bde99c137af71f95443478fc3d186480b4
Erik Carlson <carlsonerikw@gmail.com>
2016-07-31 09:40:10 -0400

    Gets HTTPResponse from web server

--- src/HNScraper.py
+++ src/HNScraper.py
@@ -3,13 +3,30 @@ from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import sys
 
-def getConfig(file):
-    return file
+def readConfig(config_file):
+    return config_file
 
-def getLinkTitle(file):
-    return file
+# Simple function to pull down the html from the web servers
+def pullHTML(url):
+    try:
+        html = urlopen(url)
+        return html
+    except HTTPError as e:
+        print("FAILURE:" + e)
+        return None
 
+def getPostLink(html):
+    return html
+
+def getPostPoints(html):
+    return html
+
+# Main function to clean up script
 def main():
+    url = "http://news.ycombinator.com/news?p=" + str(1) + ".html"
+    current_html = pullHTML(url)
+    #bsObj = BeautifulSoup(current_html, "html.parser")
+    print(current_html)
     return True
 
 main()
