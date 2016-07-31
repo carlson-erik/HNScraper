d07fcc7499c54dd2ff6750eeb100490c00a967d4
Erik Carlson <carlsonerikw@gmail.com>
2016-07-31 07:52:45 -0400

    Add start to HNScraper

---
+++ src/HNScraper.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import sys
+
+def getConfig(file):
+    return file
+
+def getLinkTitle(file):
+    return file
+
+def main():
+    return True
+
+main()
