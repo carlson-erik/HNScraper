7fc510fb18070032bb779bb47255471eae2ce19e
Erik Carlson <carlsonerikw@gmail.com>
2016-08-01 20:14:51 -0400

    Working product. Needs refining, refactoring and print clean up

--- src/HNScraper.py
+++ src/HNScraper.py
@@ -13,7 +13,7 @@ def readConfig():
         keywords.append(config['Keywords'][key].lower())
     return keywords
 
-# Simple function to pull down the html from the web servers
+# Pulls down the html from the web servers
 def pullHTML(url):
     try:
         html = 0
@@ -24,23 +24,33 @@ def pullHTML(url):
         print(e)
         return None
 
-def getAllPosts(bsoup_html):
-    return bsoup_html
+# Check to see if the title contains any of the keywords
+def checkLinkTitle(title, keywords):
+    for keyword in keywords:
+        if keyword in title:
+            return True
+    return False
 
-def getPostLink(bsoup_html):
-    return bsoup_html
-
-def getPostPoints(bsoup_html):
-    return bsoup_html
+# Scans the doc, pulls the table of posts out and stores each post's link in the array
+def buildAllPosts(bsoup_html, keywords):
+    ret_list = []
+    for post in bsoup_html.findAll("a", {"class" : "storylink"}):
+        if checkLinkTitle(post.get_text().lower(), keywords) == True:
+            cur_string = post.get_text() + " -- " + str(post['href'])
+            ret_list.append(cur_string)
+    return ret_list
 
 # Main function to clean up script
 def main():
     keywords = readConfig()
-    url = "https://news.ycombinator.com/news?p=" + str(1) + ".html"
-    print("KEYWORDS FOR SCAN:")
-    print(keywords)
-    current_html = pullHTML(url)
-    bsoup_html = BeautifulSoup(current_html, "html.parser")
-    print(bsoup_html)
+    for page_num in range(1, 40):
+        print("--------------------------------------- Page " + str(page_num) + " ---------------------------------------")
+        url = "https://news.ycombinator.com/news?p=" + str(page_num)
+        current_html = pullHTML(url)
+        bsoup_html = BeautifulSoup(current_html, "html.parser")
+        all_posts = buildAllPosts(bsoup_html, keywords)
+        for post in all_posts:
+            print(post)
+        print("--------------------------------------- Page " + str(page_num) + " ---------------------------------------")
 
 main()
